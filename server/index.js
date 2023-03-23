5ff543aaa95e115dbbff7a453b0cdcc3d504b4f4
ketusfa <ketusfa@gmail.com>
2023-03-23 18:30:01 +0300

    Add routers and modules for shop

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require("./bd");
+const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
 
 
 app.get("/", (req, res) => {
---
+++ server/routes/userRouter.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const router = new Router()
+
+router.post('/registration',)
+router.post('/login',)
+
+router.get('/auth', (req, res) => {
+    res.send('<h1>HEE</h1>')
+})
+
+module.exports = router
