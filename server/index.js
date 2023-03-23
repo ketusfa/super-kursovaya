d915b0f9a085476911ebdbdad0badab7d1ca2934
DashinIN <green19grant@yandex.ru>
2023-03-23 19:42:28 +0300

    2

--- server/index.js
+++ server/index.js
@@ -3,11 +3,8 @@ const express = require('express');
 const sequelize = require("./bd");
 const models = require('./models/models');
 const cors = require('cors');
-<<<<<<< HEAD
 const router = require('./routes/index.js');
-=======
-//const router = require('./routes/index');
->>>>>>> e77233ffd1cb92a15c59a18ea619e81e7df2a514
+
 
 const PORT = process.env.PORT || 5000;
 //прикол 
@@ -16,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-//app.use('/api', router);
+app.use('/api', router);
 
 
 
