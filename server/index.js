e76d6b4dbccc305fc7aba1f3e8f521aab1d88223
DashinIN <green19grant@yandex.ru>
2023-03-23 19:01:51 +0300

    prinyal

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const router = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
-//прикол
+//прикол 
+// otvet na prikol
 const app = express();
 
 app.use(cors());
