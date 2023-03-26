a439409c63eb4b668d3e8acd38ad4e339d832cbb
DashinIN <green19grant@yandex.ru>
2023-03-26 16:54:56 +0300

    done merge

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,17 +3,8 @@ const userController = require('../controllers/userController')
 const router = new Router()
 const UserController = require('../controllers/userController')
 
-<<<<<<< HEAD
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth', userController.check)
-=======
-router.post('/registration',)
-router.post('/login',)
-
-router.get('/auth', (req, res) => {
-    res.json({message: "ee"})
-})
->>>>>>> 09dabb157ac754abc57cd8b7668696f493a4e7c4
 
 module.exports = router;
