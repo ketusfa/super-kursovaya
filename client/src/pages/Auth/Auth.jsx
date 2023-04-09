cc2d90ee5bb686ce7b25180ed390ae425c98efe7
DashinIN <green19grant@yandex.ru>
2023-04-09 18:44:30 +0300

    start styling

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -2,32 +2,36 @@ import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import {REGISTRATION_ROUTE, LOGIN_ROUTE} from "../../utils/consts"
 
+import s from "./Auth.module.scss"
+
 const Auth = () => {
 
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE; 
     return (
-        <>
-        <h2>{isLogin ? "avtoriz" : "registration"}</h2>
+        <div className={s.auth__wrapper}>
+        <div className={s.auth__card}>
+        <h2>{isLogin ? "Авторизация" : "Регистрация"}</h2>
        
-        <form action="">
-            <input type="text" placeholder="name"/>
-            <input type="text" placeholder="passw" />
+        <form className={s.auth__form} action="">
+            <input type="text" placeholder="Ваш логин"/>
+            <input type="text" placeholder="Ваш пароль" />
             {isLogin ? 
             <div>
-                reg tut <NavLink to={REGISTRATION_ROUTE}>reeg</NavLink>
+                Зарегистрируйтесь <NavLink to={REGISTRATION_ROUTE}>тут</NavLink>
             </div>
             :
             <div>
-                vhodite tut <NavLink to={LOGIN_ROUTE}>loog</NavLink>
+                Чтобы войти перейдите <NavLink to={LOGIN_ROUTE}>сюда</NavLink>
             </div>
 
             }
             <button>
-                {isLogin ? "enter" : 'registr'}
+                {isLogin ? "Войти" : 'Зарегистрироваться'}
             </button>
         </form>
-        </>
+        </div>
+        </div>
     );
 }
 
