ef47edb94921981e8c041e3cf5a11b65bc025747
DashinIN <green19grant@yandex.ru>
2023-04-11 11:59:45 +0300

    main page and device card styled

--- client/src/components/DeviceList/DeviceList.jsx
+++ client/src/components/DeviceList/DeviceList.jsx
@@ -3,19 +3,20 @@ import {observer} from "mobx-react-lite"
 import {Context} from "../../index"
 import DeviceItem from "../DeviceItem/DeviceItem"
 
+import s from "./DeviceList.module.scss"
 
 const DeviceList = observer(() => {
         const {device} = useContext(Context)
     return (
-        <>
-        <div>
-            <div>
+        
+        <div className={s.device__wrapper}>
+            <div className={s.device__field}>
                 {device.devices.map(device => 
                     <DeviceItem key={device.id} device={device}/>
                 )}
             </div>
         </div>
-        </>
+        
     );
 });
 
