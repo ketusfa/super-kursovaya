847c63b5bf333dfb68ad722a00f38137f2ff42b1
DashinIN <green19grant@yandex.ru>
2023-04-18 17:45:25 +0300

    visual done

--- client/src/modals/CreateDevice/CreateDevice.jsx
+++ client/src/modals/CreateDevice/CreateDevice.jsx
@@ -1,6 +1,12 @@
+import React from "react";
+import {Context} from "../../index"
+
 import s from "../modals.module.scss"
 
 const CreateDevice = ({setModal, modal}) => {
+
+    const {device} = React.useContext(Context);
+
     return(
         <div className={`${s.overlay} ${modal ? s.show : "" }`}> 
         <div className={s.modal__wrapper}>
@@ -9,9 +15,30 @@ const CreateDevice = ({setModal, modal}) => {
             <button className={s.modal__close} onClick={() => setModal(false)}>close</button>
             
             <form action="" className={s.modal__form}>
-                <input type="text" placeholder="1"/>
-                <input type="text" placeholder="2 "/>
-                <button>es</button>
+                
+
+                <label >Тип устройства:</label>
+                <select>
+                    {
+                        device.types.map(type => <option key={type.id}>{type.name}</option>)
+                    }
+
+                </select>
+
+                <label >Бренд устройства:</label>
+                <select>
+                    {
+                        device.brands.map(brand => <option key={brand.id}>{brand.name}</option>)
+                    }
+
+                </select>
+                
+                <input type="text" placeholder="Навание устройства "/>
+                <input type="number" placeholder="Стоимость устройства "/>
+                <label >Изображение:</label>
+                <input type="file" placeholder="Изображение"/>
+
+                <button>Добавить</button>
             </form>
         </div>
     </div>
