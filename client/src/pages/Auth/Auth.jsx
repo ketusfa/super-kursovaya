9d788c3173a1d8fb112bf3eacb8ac032df58e679
DashinIN <green19grant@yandex.ru>
2023-04-18 18:13:00 +0300

    error db login

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import {REGISTRATION_ROUTE, LOGIN_ROUTE} from "../../utils/consts"
-import { registration } from "../../http/userAPI";
+import { login, registration } from "../../http/userAPI";
 
 import s from "./Auth.module.scss"
 
@@ -10,12 +10,16 @@ const Auth = () => {
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE; 
 
-    const click = async () => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const click = async (e) => {
+        e.preventDefault()
         if (isLogin) {
-            const response = await login();
+            const response = await login(email, password);
             console.log(response)
         } else {
-            const response = await registration();
+            const response = await registration(email, password);
             console.log(response)
         }
 
@@ -29,8 +33,8 @@ const Auth = () => {
         <h2>{isLogin ? "Авторизация" : "Регистрация"}</h2>
        
         <form className={s.auth__form} action="">
-            <input type="text" placeholder="Ваш логин"/>
-            <input type="text" placeholder="Ваш пароль" />
+            <input type="text" placeholder="Ваша почта"  value={email} onChange={e => setEmail(e.target.value)}/>
+            <input type="text" placeholder="Ваш пароль" value={password}  onChange={e => setPassword(e.target.value)}/>
             {isLogin ? 
             <div>
                 Зарегистрируйтесь <NavLink to={REGISTRATION_ROUTE}>тут</NavLink>
